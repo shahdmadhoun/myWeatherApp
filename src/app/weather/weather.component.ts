994b638f91c7da462ac73c1a95a4f209c5c6b1de
shahdmadhoun <shahdem100@gmail.com>
2022-03-05 23:18:10 -0800

    add photo

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -11,10 +11,23 @@ export class WeatherComponent implements OnInit {
   public weatherSearchForm: any;
   public weatherData: any;
   public tempCelsius = 0;
-  public tempFarenheit = 0;
   public feelCelsius = 0;
-  public feelFarenheit = 0;
+  public cityName = 'City';
+  public weatherInfo = 'Clear';
 
+  public photos: any[] = [
+    {defult: "assets/img/Clear.jfif"},
+    {Fog: "assets/img/Fog.jpg"},
+    {Clear: "assets/img/Clear.jfif"},
+    {Hail: "assets/img/Hail.jpg"},
+    {Humidity: "assets/img/Humidity.jpg"},
+    {Rain: "assets/img/Rain.jpg"},
+    {Snow: "assets/img/snow.jpg"},
+    {Storms: "assets/img/Storms.jpg"},
+    {Temperature: "assets/img/Temperature.jfif"},
+    {Wind: "../../assets/img/Wind.jfif"}
+
+  ]
 
   constructor(private formBuilder: FormBuilder, private dataService: ApiDataService) { }
 
@@ -29,6 +42,15 @@ export class WeatherComponent implements OnInit {
     .getWeather(formValues.location)
     .subscribe(data => this.weatherData = data)
     console.log(this.weatherData);
+
+    this.tempCelsius = this.weatherData?.main.temp - 273.15;
+    this.cityName = this.weatherData?.name;
+    this.feelCelsius = this.weatherData?.main.feels_like - 273.15
+    for (let index = 0; index < this.weatherData?.weather.length; index++) {
+      this.weatherInfo = this.weatherData?.weather[index].main;
+      
+    }
+
     }
 
 }
